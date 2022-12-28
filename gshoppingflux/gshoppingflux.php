fd5f43c335f56d8c6dbfb1f3e8ae24d3f5ea1103
Carmine Di Gruttola <carmine.di.gruttola@pwc.com>
2022-12-28 16:54:53 +0100

    change version

--- gshoppingflux/gshoppingflux.php
+++ gshoppingflux/gshoppingflux.php
@@ -34,7 +34,7 @@ class GShoppingFlux extends Module
     {
         $this->name = 'gshoppingflux';
         $this->tab = 'smart_shopping';
-        $this->version = '1.7.2';
+        $this->version = '1.7.4';
         $this->author = 'Dim00z';
 
         $this->bootstrap = true;
